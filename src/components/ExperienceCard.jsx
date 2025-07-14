e72c9124dec3e9f90223785df9424176a13225c2
Poorvi <poorvi.b26@gmail.com>
2025-07-13 20:58:59 -0700

    fixed css here and there, modularize experience card

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
-const ExperienceCard = ({
+const ExperienceButton = ({ buttonText, link }) => (
+  <a
+    className="text-l md:text-xl bg-[#bcc4db] text-[#8f8c60] p-1 rounded-lg border border-gray-400"
+    href={link}
+    target="_blank"
+    rel="noopener"
+  >
+    {buttonText}
+  </a>
+);
+
+const ProjectCard = ({
   imgUrl,
   name,
   date,
@@ -10,21 +21,21 @@ const ExperienceCard = ({
   previewLink,
 }) => {
   return (
-    <div className="bg-[#ffffff] text-[#575878] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-400 max-w-sm md:max-w-lg mx-auto">
+    <div className="bg-[#ffffff] text-[#575878] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-400 max-w-xs md:max-w-xl mx-auto my-4">
       <img
-        className="h-52 md:h-72 object-cover mx-auto pt-4"
+        className="h-48 md:h-80 object-cover mx-auto pt-4 pr-4 pl-4 md:mb-4"
         src={imgUrl}
         alt=""
       />
-      <div className="text-[#8f8c60] italic ml-5 md:ml-4 mt-2">
-        <p className="font-bold text-l md:text-xl">{name}</p>
-        <p className="font-semibold text-sm md:text-base">{date}</p>
+      <div className="text-[#8f8c60] italic md:ml-4 pt-4 pl-4 pr-4 md:p-0">
+        <p className="font-bold text-l md:text-2xl">{name}</p>
+        <p className="font-semibold text-sm md:text-lg">{date}</p>
       </div>
-      <p className="mt-2 text-sm md:text-base w-[90%] md:w-[94%] mx-auto">
+      <p className="mt-2 text-sm md:text-lg w-[90%] md:w-[94%] mx-auto">
         {description}
       </p>
-      <div className="flex mt-3 items-center mb-3 justify-around gap-6 md:gap-20">
-        <div className="flex gap-2 w-[60%] text-xs md:text-sm flex-wrap justify-start">
+      <div className="flex my-3 items-center justify-around gap-6 md:gap-20 md:px-4 px-1">
+        <div className="flex gap-2 w-[60%] text-sm md:text-base flex-wrap justify-start">
           {techStack.map((item, idx) => (
             <div
               className="bg-[#ffffff] backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-400 p-[2px] rounded-lg "
@@ -34,24 +45,81 @@ const ExperienceCard = ({
             </div>
           ))}
         </div>
-        {gitHubLink && (
-          <a
-            className="text-l md:text-xl bg-[#bcc4db] text-[#8f8c60] p-1 rounded-lg border border-gray-400"
-            href={gitHubLink}
-            target="_blank"
-            rel="noopener"
-          >
-            GitHub
-          </a>
-        )}
-        {previewLink && (
-          <a className="" href={previewLink} target="_blank" rel="noopener">
-            Preview
-          </a>
-        )}
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+          {gitHubLink && (
+            <ExperienceButton buttonText="Github" link={gitHubLink} />
+          )}
+          {previewLink && (
+            <ExperienceButton buttonText="Preview" link={previewLink} />
+          )}
+        </div>
       </div>
     </div>
   );
+};
+
+const ReseachOtherCard = ({ imgUrl, name, date, description }) => {
+  return (
+    <div className="bg-[#ffffff] text-[#575878] rounded-xl backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-400 max-w-xs md:max-w-xl mx-auto my-4">
+      <div className="flex items-center md:mt-4 mb-3">
+        <img
+          className="h-10 md:h-16 object-cover mx-auto pt-4 pr-4 pl-4 md:mb-4"
+          src={imgUrl}
+          alt=""
+        />
+        <div className="text-[#8f8c60] italic md:ml-4 pt-4 pl-4 pr-4 md:p-0">
+          <p className="font-bold text-l md:text-2xl">{name}</p>
+          <p className="font-semibold text-sm md:text-lg">{date}</p>
+        </div>
+      </div>
+      <ul className="mt-2 text-sm md:text-lg w-[80%] md:w-[90%] mx-auto mb-4 space-y-2">
+        {description.map((item, idx) => (
+          <li className="list-disc" key={idx}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ExperienceCard = ({
+  imgUrl,
+  name,
+  date,
+  description,
+  techStack,
+  gitHubLink,
+  previewLink,
+  type,
+}) => {
+  switch (type) {
+    case "project":
+      return (
+        <ProjectCard
+          imgUrl={imgUrl}
+          name={name}
+          date={date}
+          description={description}
+          techStack={techStack}
+          gitHubLink={gitHubLink}
+          previewLink={previewLink}
+        />
+      );
+
+    case "research":
+      return (
+        <ReseachOtherCard
+          imgUrl={imgUrl}
+          name={name}
+          date={date}
+          description={description}
+        />
+      );
+
+    case "other":
+      break;
+  }
 };
 
 export default ExperienceCard;
--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ExperienceCard from "./ExperienceCard";
-import ExampleImage from "../assets/example.png";
+import CampusConnect from "../assets/campusConnect.png";
+import RosieLab from "../assets/rosielab.png";
 
 const ExperienceSection = () => {
   const experienceTypes = ["projects", "research", "other"];
@@ -13,7 +14,7 @@ const ExperienceSection = () => {
       >
         <p className="font-extrabold italic text-center ">experience —</p>
 
-        <div className="flex space-x-10 font-extrabold ml-6">
+        <div className="flex justify-center space-x-7 font-extrabold md:ml-6">
           {experienceTypes.map((label, idx) => (
             <button key={idx} className="hover:text-[#8f8c60] cursor-pointer">
               {label}
@@ -22,10 +23,10 @@ const ExperienceSection = () => {
         </div>
       </div>
       <ExperienceCard
-        imgUrl={ExampleImage}
+        imgUrl={CampusConnect}
         name="Campus Connect AI"
         date="Mar. 2025 - May 2025"
-        description="Local Knowledge RAG Chat bot for International Students as part of RBC Borealis’ Let’s Solve It 2025"
+        description="Local Knowledge RAG Chat bot for International Students as part of RBC Borealis' Let's Solve It 2025"
         techStack={[
           "Streamlit",
           "Python",
@@ -34,8 +35,31 @@ const ExperienceSection = () => {
           "Mistral-7B",
         ]}
         gitHubLink="https://github.com/ayanahye/Campus-Connect-AI"
-        previewLink={null}
+        previewLink={"https://google.com"}
+        type="project"
       ></ExperienceCard>
+      <ExperienceCard
+        imgUrl={RosieLab}
+        name="Research Assistant at Rosie Lab, SFU"
+        date="Dec. 2025 - Cur."
+        description={[
+          "Benchmarked automatic speech recognition (ASR) models from OpenAI and Nvidia on untrained audio types, such as distanced and shouted speech, to evaluate performance gaps and model robustness",
+          "Utilized PyTorch and Pandas to preprocess audio datasets, train models, and analyze results, gaining hands-on experience with deep learning frameworks and data manipulation.",
+        ]}
+        type="research"
+      />
+      <ExperienceCard
+        imgUrl={
+          "https://ixlab.cs.sfu.ca/static/img/siteAssets/globalAssets/ixlab.svg"
+        }
+        name="USRA at ixlab, SFU"
+        date="May. 2025 - Aug. 2025"
+        description={[
+          "Benchmarked automatic speech recognition (ASR) models from OpenAI and Nvidia on untrained audio types, such as distanced and shouted speech, to evaluate performance gaps and model robustness",
+          "Utilized PyTorch and Pandas to preprocess audio datasets, train models, and analyze results, gaining hands-on experience with deep learning frameworks and data manipulation.",
+        ]}
+        type="research"
+      />
     </>
   );
 };
